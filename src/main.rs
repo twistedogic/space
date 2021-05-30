61133a24289884e43e8b8db78c01d26410ab5774
Jordan Li <twistedogic@gmail.com>
2021-05-30 14:25:40 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use url::{Url, Position, ParseError};
+
+
+const MIDLAND_LINK: &str = "https://data.midland.com.hk/search/v1/properties?hash=false&lang=en&currency=HKD&unit=feet&search_behavior=normal&tx_type=S&page=1&limit=24"
+const GOV_LINK: &str = "https://data.gov.hk/assets/data/geospatial/geo_res_metadata.json"
+const GOV_LINK: &str = "https://data.gov.hk/assets/data/geospatial/geo_res_metadata.json"
+const GOV_LINK: &str = "https://data.gov.hk/assets/data/geospatial/geo_res_metadata.json"
+
+fn main() {
+    println!("Hello, world!");
+}
